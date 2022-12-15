e90182f3c8d32730d4351c72dcb8a32ff0fe7ee9
SahsenemOzer <sahsenemozer1@gmail.com>
2022-12-15 23:38:07 +0300

    template giydirme

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from unicodedata import category
+
 
 # Create your views here.
 
 def index(request):
     text="Merhaba Dünya"
-    context={'text':text}
+    context={'text':text,
+             'category': category}
     return render(request, 'index.html', context)
